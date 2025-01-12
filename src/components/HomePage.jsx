3e30d2208281042a7e72cd0029db051f3f6a47ae
AvvaruDeepak <avvarudeepak123@gmail.com>
2025-01-12 16:22:21 +0530

    todo-app

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,24 @@
-import TodoTasks from "./Todotasks"
+import TodoTasks from "./TodoTasks"
 import OngoingTasks from "./OngoingTasks"
 import CompletedTasks from "./CompletedTasks"
+import {useState} from "react"
 
 export default function HomePage()
 {
+    const [task,setTask]=useState("")
+    const [click, setClick]=useState(false)
+    function submit()
+    {
+        alert(task)
+        
+    }
+    
     return(
         <>
             <div className="home">
-                <form className="task-form">
-                    <input type="text" required className="task-input" placeholder="Enter Task"></input>
+                <form className="task-form" onSubmit={submit}>
+                    <input type="text" required className="task-input" placeholder="Enter Task" 
+                        onChange={(e) => setTask(e.target.value)}></input>
                     <button className="add-task-button">ADD TASK</button>
                 </form>
                 <div className="task-sections">
